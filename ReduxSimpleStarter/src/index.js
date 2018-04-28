f810ff23464fc1f7234f2873e85112d213340936
Kelvin Hong <kelvinok52@gmail.com>
2018-04-28 14:30:02 +0700

    react training init

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBbkqhjQcbohB-iB4QhISw5YICaodr1zsQ';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
+
+// Take this component's generated HTML and put it on page
+ReactDOM.render(<App />, document.querySelector('.container'));
