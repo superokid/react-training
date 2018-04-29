df369538f4fdf03c7fd65811d92d1eae0099f619
Kelvin Hong <kelvinok52@gmail.com>
2018-04-29 13:18:31 +0700

    redux actions

---
+++ redux-book-selector/src/actions/index.js
@@ -0,0 +1,7 @@
+export function selectBook(book) {
+  // selectbook is an action creator, it needs to return an action, an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
+}
