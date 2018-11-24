eaec9a5edc0220a803c1bc6be81a5e16f5856dfb
superokindonesia <kelvinok52@gmail.com>
2018-11-24 12:07:46 +0700

    blog

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -2,7 +2,7 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
   switch (action.type) {
-    case 'FETCH_WEATHER':
+    case FETCH_WEATHER:
       return [ action.payload.data, ...state ];
   }
   return state;
